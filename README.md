b4fc8b42a94f561844809def3638244ae49a7e8d
Fadhil Ahsan <fadhilahsan2104@gmail.com>
2019-10-01 00:08:52 +0700

    add error message, add readme.md for instruction

---
+++ README.md
@@ -0,0 +1,16 @@
+# website-health-checker
+
+website for check health from a URL
+
+Version Golang (go1.13)
+
+# How To Run 
+
+1. Install Golang, you can visit [This Link](https://golang.org/doc/install)
+3. Open your terminal then execute command `git clone https://github.com/FadhilAhsan/website-health-checker.git` in your `$GOPATH/src`
+4. Go to folder `github.com/FadhilAhsan/website-health-checker/cmd/website-health-checker/`
+5. For run this project :
+	* Open your terminal then execute command`go run main.go` or
+	* Open your terminal then execute command`go build` and run execute file
+6. Open Browser go to address `localhost:PORT`
+
--- internal/app/website-health-checker/controllers/urlMonitorController.go
+++ internal/app/website-health-checker/controllers/urlMonitorController.go
@@ -5,31 +5,34 @@ import (
   "net/http"
   "github.com/FadhilAhsan/website-health-checker/internal/app/website-health-checker/services"
   "github.com/FadhilAhsan/website-health-checker/internal/app/website-health-checker/utils"
+  "github.com/FadhilAhsan/website-health-checker/internal/app/website-health-checker/models"
 )
 
 func URLMonitorPage(w http.ResponseWriter, r *http.Request) {
 	log.Println("URLMonitor Page")
     
-	
+	pageDate := models.URLMonitorPage{"Web Health Checker","",models.URLMonitors{}}
 	if r.Method == "POST" {
 		err := r.ParseForm()
 		if err != nil {
             log.Println("[Error] URLMonitorPage ParseForm() err : ", err)
-            return
+            pageDate.ErrorMessage = "Input Invalid."
         }
         urlString := r.FormValue("url")
         err = services.AddURL(urlString)
         if err != nil {
         	log.Println("[Error] URLMonitorPage AddURL err : ", err)
-            return
+          pageDate.ErrorMessage = "Input Invalid."
         }
 	}
 
   urls,err := services.GetAllURL()
 	if err != nil {
-		return 
-	}
+		pageDate.ErrorMessage = "Can't load data." 
+	}else {
+    pageDate.URLs = urls
+  }
 
-  utils.RenderTemplate(w,"urlmonitor",urls)
+  utils.RenderTemplate(w,"urlmonitor",pageDate)
 }
 
